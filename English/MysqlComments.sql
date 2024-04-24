388362aafd96123bfce134c82fcbd107cf76b427
Kanagamani <Kanagamani.T@cognizant.com>
2024-04-24 18:20:14 +0530

    added
    added

---
+++ English/MysqlComments.sql
@@ -0,0 +1,24 @@
+/*
+create database School;
+*/
+/*use School;
+create table Class
+(classId int primary key, 
+className varchar(32) not null
+) ;
+*/
+/*use School;
+insert into class values (1, 'A');
+insert into class values (2, 'B');
+insert into class values (3, 'C');
+*/
+/*select * from class;*/
+
+/*use school;
+create table student (
+studentId int primary key,
+studentName varchar(32) not null,
+classId
+
+);
+*/
